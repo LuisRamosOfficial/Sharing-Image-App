5c6bc9544889fb25abe21b1a94bca0917f0b404d
Luis Ramos <120321858+LuisRamosOfficial@users.noreply.github.com>
2023-01-26 00:05:53 +0000

    Configuration

---
+++ src/assets/Components/FirebaseConfig.tsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBdaKXmALauwa5-UJw9saYjOEUUpeLxW8E',
+	authDomain: 'sharing-image-app.firebaseapp.com',
+	projectId: 'sharing-image-app',
+	storageBucket: 'sharing-image-app.appspot.com',
+	messagingSenderId: '461014017878',
+	appId: '1:461014017878:web:2118cb9ed67d35efc718f7',
+};
+
+export default firebaseConfig
